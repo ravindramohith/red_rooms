9dbb0dc529d94ffb45b8678a997f49d7e642f32c
ravindramohith <ravindramohith@gmail.com>
2022-12-16 04:42:20 +0530

    Removed Sensitive info

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
+const { CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET, CLOUDINARY_CLOUD_NAME } = require('./secrets/secret')
 const nextConfig = {
   env: {
     DB_LOCAL_URI: "mongodb://localhost:27017/RedRooms",
     NEXTAUTH_URL: "http://localhost:3000/",
-    CLOUDINARY_CLOUD_NAME: "ravindramohith",
-    CLOUDINARY_API_KEY: "342594479789283",
-    CLOUDINARY_API_SECRET: "LpjrvJyRM3fmIG3BJb8bDmrL1V0",
+    CLOUDINARY_CLOUD_NAME,
+    CLOUDINARY_API_KEY,
+    CLOUDINARY_API_SECRET
   },
   images: {
     domains: ['res.cloudinary.com', 'a0.muscache.com']
